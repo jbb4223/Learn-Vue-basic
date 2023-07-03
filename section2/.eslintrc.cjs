8765ec7006a49d194e2b41bcc6a16dfe1b73b7d9
jbb4223 <jbb4223_3top@gsretail.com>
2023-07-03 16:32:58 +0900

    section2 unnecessary file remove

--- section2/.eslintrc.cjs
+++ section2/.eslintrc.cjs
@@ -8,7 +8,20 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+  'env': {
+    'vue/setup-compiler-macros': true
+  },
+  'rules': {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": ["error", {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+    }]
+  },
 }
