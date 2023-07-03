40186df45d3b69735a321c4b63314691e1fdc26f
jbb4223 <jbb4223_3top@gsretail.com>
2023-07-03 15:32:54 +0900

    section 1 - ready

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+export default {
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+};
