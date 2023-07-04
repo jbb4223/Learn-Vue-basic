32816bc42ec92b834fdc9f889a173289d2fc20ae
jbb4223 <jbb4223_3top@gsretail.com>
2023-07-04 17:05:14 +0900

    Computed

---
+++ section2/src/lecture/8/App_computed.vue
@@ -0,0 +1,63 @@
+<template>
+	<div>
+		<h2>{{ teacher.name }}</h2>
+		<h3>강의가 있습니까?</h3>
+		<p>{{ hasLecture }}</p>
+		<p>{{ hasLecture }}</p>
+		<p>{{ existLecture() }}</p>
+		<p>{{ existLecture() }}</p>
+		<button v-on:click="counter++">Counter: {{ counter }}</button>
+		<h2>이름</h2>
+		<p>{{ fullName }}</p>
+	</div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const teacher = reactive({
+			name: '박진병',
+			lectures: ['HTML/CSS', 'JavaScript', 'Vue3'],
+		});
+
+		// 여러번 호출을 해도 캐쉬가 됨
+		const hasLecture = computed(() => {
+			console.log('computed');
+			return teacher.lectures.length > 0 ? '있음' : '없음';
+		});
+
+		// 여러번 호출을 해서 성능에 좋지 않음
+		const existLecture = () => {
+			console.log('method');
+			return teacher.lectures.length > 0 ? '있음' : '없음';
+		};
+
+		const counter = ref(0);
+
+		const firstName = ref('홍');
+		const lastName = ref('길동');
+
+		const fullName = computed({
+			get() {
+				return firstName.value + ' ' + lastName.value;
+			},
+			set(value) {
+				[firstName.value, lastName.value] = value.split(' ');
+			},
+		});
+		console.log('console 출력 : ', fullName.value);
+		fullName.value = '짐 코딩';
+		return {
+			teacher,
+			hasLecture,
+			existLecture,
+			counter,
+			fullName,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
