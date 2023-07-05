328cbe6a843994807fe65346b5cf62228fdded66
jbb4223 <jbb4223_3top@gsretail.com>
2023-07-05 15:30:21 +0900

    v-model

---
+++ section2/src/lecture/14/App_two_way_binding.vue
@@ -0,0 +1,93 @@
+<template>
+	<div>
+		<h2>input Value</h2>
+		<!--		<input type="text" v-model.lazy="inputValue" />-->
+		<!--		<input type="text" v-model.number="inputValue" />-->
+		<input type="text" v-model.trim="inputValue" />
+		<div>{{ inputValue }}</div>
+		<div>{{ typeof inputValue }}</div>
+
+		<hr />
+
+		<h2>textarea value</h2>
+		<textarea v-model="textareaValue"></textarea>
+		<div>{{ textareaValue }}</div>
+
+		<hr />
+
+		<h2>checkbox value</h2>
+		<label for="checkbox">{{ checkboxValue }}</label>
+		<input
+			type="checkbox"
+			id="checkbox"
+			v-model="checkboxValue"
+			true-value="Yes"
+			false-value="NO"
+		/>
+		<h3>checkbox values</h3>
+		<label>
+			<input type="checkbox" value="html" v-model="checkboxValues" />
+			HTML
+		</label>
+		<label>
+			<input type="checkbox" value="css" v-model="checkboxValues" />
+			CSS
+		</label>
+		<label>
+			<input type="checkbox" value="javascript" v-model="checkboxValues" />
+			JavaScript
+		</label>
+		<div>{{ checkboxValues }}</div>
+		<hr />
+
+		<h2>radio value</h2>
+		<label>
+			<input type="radio" name="type" value="O" v-model="radioValue" />
+			O형
+		</label>
+		<label>
+			<input type="radio" name="type" value="A" v-model="radioValue" />
+			A형
+		</label>
+		<div>
+			{{ radioValue }}
+		</div>
+
+		<hr />
+
+		<h2>select value</h2>
+		<select v-model="selectValue">
+			<option value="html">HTML 수업</option>
+			<option value="css">CSS 수업</option>
+			<option value="javascript">JavaScript 수업</option>
+		</select>
+		<div>{{ selectValue }}</div>
+
+		<hr />
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const inputValue = ref(null);
+		const textareaValue = ref(null);
+		const checkboxValue = ref('Yes');
+		const checkboxValues = ref([]);
+		const radioValue = ref('O');
+		const selectValue = ref('html');
+		return {
+			inputValue,
+			textareaValue,
+			checkboxValue,
+			checkboxValues,
+			radioValue,
+			selectValue,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
