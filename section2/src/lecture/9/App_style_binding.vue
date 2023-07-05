28fb525aceb142090127e7cd3ddfe588ac5afc12
jbb4223 <jbb4223_3top@gsretail.com>
2023-07-05 10:22:51 +0900

    class and style binding

---
+++ section2/src/lecture/9/App_style_binding.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+			accusantium, adipisci aspernatur assumenda culpa delectus deleniti
+			deserunt dignissimos eligendi est libero maiores, nisi non numquam
+			pariatur repellat repellendus, sit voluptatibus!
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return {
+			fontSize,
+			styleObject,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
