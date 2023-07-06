7615977b003cd9f449c5a11ab64bd0ecc00a3165
jbb4223 <jbb4223_3top@gsretail.com>
2023-07-06 14:09:09 +0900

    Props

--- section2/src/components/AppCard.vue
+++ section2/src/components/AppCard.vue
@@ -1,46 +1,65 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title red">Card title</h5>
+			<!--   type: news, notice   -->
+			<span class="badge bg-secondary">
+				{{ typeName }}
+			</span>
+			<h5 class="card-title red mt-2">
+				{{ title }}
+			</h5>
 			<p class="card-text">
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
+				{{ contents }}
 			</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
+			<a class="btn" :class="isLikeClass" @click.prevent="toggleLike">좋아요</a>
 		</div>
 	</div>
 </template>
 
 <script>
-import { ref, useCssModule } from 'vue';
+import { computed } from 'vue';
 
-console.log('AppCard Module');
 export default {
-	setup() {
-		// const style = useCssModule();
-		// console.log('style : ', style);
-		console.log('AppCard setup()');
-
-		const color = ref('green');
-		return { color };
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+			validator: value => {
+				return ['news', 'notice'].includes(value);
+			},
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false,
+		},
+		obj: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	emits: ['toggleLike'],
+	setup(props, context) {
+		const isLikeClass = computed(() =>
+			props.isLike ? 'btn-danger' : 'btn-outline-danger',
+		);
+		const typeName = computed(() =>
+			props.type === 'news' ? '뉴스' : '공지사항',
+		);
+		const toggleLike = () => {
+			// props.isLike = !props.isLike;
+			context.emit('toggleLike');
+		};
+		return { isLikeClass, typeName, toggleLike };
 	},
 };
 </script>
 
-<style>
-.red {
-	color: v-bind(color) !important;
-}
-</style>
-
-<!--<style module="classes">
-.red {
-	color: red !important;
-}
-</style>-->
-
-<!--<style scoped>
-.red {
-	color: red !important;
-}
-</style>-->
+<style></style>
