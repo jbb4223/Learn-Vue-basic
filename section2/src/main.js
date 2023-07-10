8c00a45f01940d9bd925e2bc1c801e60686a8261
jbb4223 <jbb4223_3top@gsretail.com>
2023-07-10 13:57:28 +0900

    Provice / Inject part 2 , Lifecycle Hooks

--- section2/src/main.js
+++ section2/src/main.js
@@ -4,5 +4,9 @@ import App from '@/App.vue';
 
 const app = createApp(App);
 
+app.provide('app-message', 'app message 입니다.');
+// 모든 컴포넌트에서 접근하고 싶은 객체를 넣을때
+app.config.globalProperties.msg = 'hello';
+app.provide('msg', 'hello msg');
 app.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
